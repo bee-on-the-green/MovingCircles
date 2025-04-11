ca7c568274dd9c2e4838fcb55052700d1880826f
bee-on-the-green <bee_on_the_fly@proton.me>
2025-04-11 19:19:46 +0200

    new circle main implemented

--- app/src/main/java/com/example/movingcircles/MatrixInitializerCircle.kt
+++ app/src/main/java/com/example/movingcircles/MatrixInitializerCircle.kt
@@ -4,10 +4,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class MatrixInitializerCircle {
-    private val MatrixLength: Int = 20  // Unique to squareSmalls
+    private val MatrixLength: Int = 100  // Unique to squareSmalls
     private val MatrixHeight: Int = MatrixLength * 42 / 100  // 55% aspect ratio
-    private val poolOfChars: Array<Char> = arrayOf('.', '·')  // Unique character pool
-// private val squareSmallPoolOfChars: Array<Char> = arrayOf('.', '·')  // Unique character pool
+    private val poolOfCharsInit: Array<Char> = arrayOf(' ', ' ')  // Unique character pool ·
+
 
 
 
@@ -17,7 +17,7 @@ class MatrixInitializerCircle {
     ): MutableList<MutableList<Char>> {
         return withContext(Dispatchers.Default) {
             MutableList(rows) {
-                MutableList(cols) { poolOfChars.random() }
+                MutableList(cols) { poolOfCharsInit.random() }
             }
         }
     }
@@ -25,25 +25,3 @@ class MatrixInitializerCircle {
 
 
 
-/*
-
-class MatrixInitializerSquareSmall {
-    private val squareSmallMatrixLength: Int = 20  // Unique to squareSmalls
-    private val squareSmallMatrixHeight: Int = squareSmallMatrixLength * 42 / 100  // 55% aspect ratio
-    private val poolOfChars: Array<Char> = arrayOf('.', '·')  // Unique character pool
-// private val squareSmallPoolOfChars: Array<Char> = arrayOf('.', '·')  // Unique character pool
-
-
-
-    suspend fun initializeMatrix(
-        rows: Int = squareSmallMatrixHeight,
-        cols: Int = squareSmallMatrixLength
-    ): MutableList<MutableList<Char>> {
-        return withContext(Dispatchers.Default) {
-            MutableList(rows) {
-                MutableList(cols) { poolOfChars.random() }
-            }
-        }
-    }
-}
-*/
